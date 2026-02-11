f36d5b61719577b57e5cb57bd06063d4c7e632c2
ATHARVA-DESHMUKH-23 <atharvad2366@gmail.com>
2026-02-11 22:16:27 +0530

    task1a simulated

---
+++ ebot_description/launch/localization.launch.py
@@ -0,0 +1,47 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    map_file = '/home/rajvardhan/mobile_manipulator/map/office_map.yaml'
+
+    return LaunchDescription([
+
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
+            output='screen',
+            parameters=[{'yaml_filename': map_file}]
+        ),
+
+        Node(
+            package='nav2_amcl',
+            executable='amcl',
+            name='amcl',
+            output='screen',
+            parameters=[{
+                'use_sim_time': False,
+                'base_frame_id': 'ebot_base_link',
+                'odom_frame_id': 'odom',
+                'scan_topic': '/scan',
+                'transform_tolerance': 0.5,
+            }]
+        ),
+
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_localization',
+            output='screen',
+            parameters=[{
+                'use_sim_time': False,
+                'autostart': True,
+                'node_names': ['map_server', 'amcl']
+            }]
+        )
+    ])
